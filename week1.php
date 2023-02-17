91cf6a64401010c88e7e45326d42e636b83e968d
Norofuru <angel920731@gmail.com>
2023-02-17 15:58:03 +0800

    add pages

---
+++ week1.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>week1</title>
+    </head>
+
+    <body>
+        <?php
+            echo "test<br/>";
+            echo "你好";
+        ?>
+    </body>
+</html>
